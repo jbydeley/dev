57534a963242693914249a0207b8d3644bab4469
Jared Bydeley <jared@roadmunk.com>
2021-02-14 09:53:52 -0500

    Debug CI/CD

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -46,5 +46,3 @@
 	<img alt="Success Kid" src="{successkid}">
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
